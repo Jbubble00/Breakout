4c6a6c4dd59b79b07d73fff502a9a30d4a679229
Jbubble00 <jfuentes3190@stu.neisd.net>
2015-10-21 10:54:05 -0500

    Breakout Changes
    Changes

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	public float paddlespeed = 1f;
+
+
+	private Vector3 playerPos = new Vector3 (0, -9.5f, 0);
+
+	// Update is called once per frame
+	void Update () 
+	{
+		float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddlespeed);
+		playerPos = new Vector3 (Mathf.Clamp (xPos, -8f, 8f), -9.5f, 4.25f);
+		transform.position = playerPos;
+
+	}
+}
